b8b60bf339c9cd633ed59e57968ba23f935b1e01
piotrwiatr <peterwiatr6235@gmail.com>
2025-01-25 21:06:20 -0500

    added hamburger menu for mobile support

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-// Smooth scrolling functionality using JavaScript
+let determineNavType = () => {
+  let nav = document.querySelector('#navbar');
+  let mobileNav = document.querySelector('#mobile-nav');
+  if (window.innerWidth <= 920) {
+    nav.style.display = "none";
+    mobileNav.style.display = "flex";
+  } else {
+    nav.style.display = "flex";
+    mobileNav.style.display = "none";
+  }
+};
+
+document.addEventListener("DOMContentLoaded", determineNavType)
+
+window.addEventListener("resize", determineNavType);
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+    anchor.addEventListener('click', (e) => {
         e.preventDefault();
         const targetElement = 
-              document.querySelector(this.getAttribute('href'));
+              document.querySelector(e.target.getAttribute('href'));
         window.scrollTo({
             top: targetElement.offsetTop - 90,
             behavior: 'smooth'
         });
+
+        if (e.target.parentNode.id === "mobile-navigation") {
+            displayNav();
+        }
     });
 });
+
+let displayNav = () => {
+    var x = document.querySelector("#nav-links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
